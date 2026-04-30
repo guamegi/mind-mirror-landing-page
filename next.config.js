01de7835f6a1b6d6bbf63d3ae44c52f8b0bd4e09
gumna <dev.namikon@gmail.com>
2026-04-30 17:51:15 +0900

    path 설정 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: '/mind-mirror-landing-page',
+  assetPrefix: '/mind-mirror-landing-page',
 }
 
 module.exports = nextConfig
